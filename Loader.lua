53be67821a38a938b84fc124ed30f76ac34a5bed
LucasggkX <iranidesouza29@gmail.com>
2025-09-05 14:10:26 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ local id = game.PlaceId
 local raw
 
 if id == 126509999114328 then
-  raw = ""
+  raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/99%20Nights%20in%20the%20forest/Protected_1235531579188194.lua"
 else
   return
 end
