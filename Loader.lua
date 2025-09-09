7c17b24ada8393ae3ca0a951eb13033195a77aef
LucasggkX <iranidesouza29@gmail.com>
2025-09-09 16:22:18 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -11,8 +11,8 @@ if id == 126509999114328 then
 elseif id == 109983668079237 then
   game.Players.LocalPlayer:Kick("I'm fixing some bugs! The script is currently disabled. Use 99 Nights script!")
   setclipboard("lucasggkx.github.io/LKZ-Hub")
-  return
   raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/Steal%20a%20brainrot/Protected_7857289872673061.lua"
+  return
 else 
   return
 end
