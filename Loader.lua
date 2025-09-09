f3b1926f56de4b21cfefa2efb5f2f984f79420a3
LucasggkX <iranidesouza29@gmail.com>
2025-09-09 16:19:50 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,6 +9,9 @@ local raw
 if id == 126509999114328 then
   raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/99%20Nights%20in%20the%20forest/Protected_1235531579188194.lua"
 elseif id == 109983668079237 then
+  game.Players.LocalPlayer:Kick("I'm fixing some bugs! The script is currently disabled. Use 99 Nights script!")
+  setclipboard("lucasggkx.github.io/LKZ-Hub")
+  return
   raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/Steal%20a%20brainrot/Protected_7857289872673061.lua"
 else 
   return
