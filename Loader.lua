26b843fea4020c7638459aa79ab687a3c714f932
LucasggkX <iranidesouza29@gmail.com>
2025-09-12 15:51:42 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,20 +2,15 @@ if _G.exec then return end _G.exec = true
 local id = game.PlaceId; local raw
 function Add() print("execution of script number: " .. game:GetService("HttpService"):JSONDecode(request({Url="https://api.counterapi.dev/v2/lkz/lkz-hub/up", Method="GET"}).Body).data.up_count / 2) end
 
-if id == 126509999114328 then
-  
+if id == 126509999114328 then  
   raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/99%20Nights%20in%20the%20forest/Protected_1235531579188194.lua"
-  setclipboard("lucasggkx.github.io/LKZ-Hub")
-  
-elseif id == 109983668079237 then
-  
+elseif id == 109983668079237 then  
   raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/Steal%20a%20brainrot/Protected_7857289872673061.lua"
-  game.Players.LocalPlayer:Kick("I'm fixing some bugs! The script is currently disabled. Use 99 Nights script!")
-  setclipboard("lucasggkx.github.io/LKZ-Hub")
-  return
-
-else return end
+else 
+  return 
+end
 
 Add()
+setclipboard("lucasggkx.github.io/LKZ-Hub")
 loadstring(game:HttpGet(raw))()
 
