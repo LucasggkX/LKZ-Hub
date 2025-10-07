4012ddd03e75dee9e601ceab741da644bcceb41e
LucasggkX <iranidesouza29@gmail.com>
2025-10-07 18:43:22 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,7 +9,7 @@ function Add() print("execution of script number: " .. game:GetService("HttpServ
 
 if id == 126509999114328 then  
   raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/99%20Nights%20in%20the%20forest/Protected_1235531579188194.lua"
-elseif id == 109983668079237 or id == 128762245270197 then  
+elseif id == 109983668079237 or id == 128762245270197 or id == 96342491571673 then  
   raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/Steal%20a%20brainrot/Protected_7857289872673061.lua"
 else 
   return 
