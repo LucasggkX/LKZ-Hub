f52b3cd47812d0d13b2d9884cbdead2b37289604
LucasggkX <iranidesouza29@gmail.com>
2025-10-10 13:25:34 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -10,6 +10,7 @@ local id = game.PlaceId
 local raw
 
 -- function Add() local s, r = pcall(function() return request({Url="https://api.counterapi.dev/v2/lkz/lkz-hub/up", Method="GET"}) end) if s and r then local d = game:GetService("HttpService"):JSONDecode(r.Body) if d and d.data and d.data.up_count then d.data.up_count = d.data.up_count/2 end end end
+-- Add()
 
 if id == 126509999114328 then  
     raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/99%20Nights%20in%20the%20forest/Protected_1235531579188194.lua"
@@ -19,5 +20,4 @@ else
     return 
 end
 
--- Add()
 loadstring(game:HttpGet(raw))()
