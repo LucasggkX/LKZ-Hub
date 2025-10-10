70b228dd30a6de02d785c12a54af63e2242a1893
LucasggkX <iranidesouza29@gmail.com>
2025-10-10 14:17:51 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,8 +9,6 @@ _G.exec = true
 local id = game.PlaceId
 local raw
 
-function Add() request({Url="https://counter.lucasemanuelguimaraes20.workers.dev/up", Method="POST", Headers={["Content-Type"]="application/json"}}) end
-
 if id == 126509999114328 then  
     raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/99%20Nights%20in%20the%20forest/Protected_1235531579188194.lua"
 elseif id == 109983668079237 or id == 128762245270197 or id == 96342491571673 then  
@@ -19,5 +17,4 @@ else
     return 
 end
 
-Add()
 loadstring(game:HttpGet(raw))()
