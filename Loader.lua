b73ff955bc94019668999af7266abad88e50bbc7
LucasggkX <iranidesouza29@gmail.com>
2025-10-11 23:20:57 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -12,7 +12,7 @@ local raw
 function Add() local s, r = pcall(function() return request({Url="https://api.counterapi.dev/v2/lkz/lkz-hub/up", Method="GET"}) end) if s and r then local d = game:GetService("HttpService"):JSONDecode(r.Body) if d and d.data and d.data.up_count then d.data.up_count = d.data.up_count/2 end end end
 
 if id == 126509999114328 then  
-    raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/99%20Nights%20in%20the%20forest/Principal.lua"
+    raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/99%20Nights.lua"
 elseif id == 109983668079237 or id == 128762245270197 or id == 96342491571673 then  
     raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/Steal%20a%20brainrot/Principal.lua"
 else 
