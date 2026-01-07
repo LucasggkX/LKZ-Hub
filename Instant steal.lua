0431ad6aea24d974264172a406d408f39f77ecf4
LucasggkX <220381363+LucasggkX@users.noreply.github.com>
2026-01-07 09:50:42 -0300

    Create Instant steal.lua

---
+++ Instant steal.lua
@@ -0,0 +1,4 @@
+if setclipboard then
+    setclipboard("https://discord.gg/RVFUypzgHP")
+end
+game.Players.LocalPlayer:Kick("Script Patched, Thank you for using :)\nDiscord: https://discord.gg/RVFUypzgHP")
