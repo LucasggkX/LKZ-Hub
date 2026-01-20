f0b3d432b302e37a588ebcf033bcce930b24a036
LucasggkX <220381363+LucasggkX@users.noreply.github.com>
2026-01-20 11:02:22 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,7 +9,7 @@ function Add() pcall(function() (request or http_request or (syn and syn.request
 if id == 126509999114328 then  
     raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/99%20Nights.lua"
 elseif id == 109983668079237 or id == 128762245270197 or id == 96342491571673 then  
-    raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/Steal%20a%20brainrot.lua"
+    raw = "https://api.luarmor.net/files/v4/loaders/66705d62b53668c135171165c745016a.lua"
 elseif id == 127742093697776 then
     raw = "https://raw.githubusercontent.com/LucasggkX/Games/refs/heads/main/Plant%20vs.lua"
 elseif id == 537413528 then
