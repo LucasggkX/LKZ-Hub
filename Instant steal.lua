9e6c4dc5cd8a9a23fcddc5c175d4c0ebf1a8fee8
LucasggkX <220381363+LucasggkX@users.noreply.github.com>
2026-01-20 11:19:14 -0300

    Delete Instant steal.lua

--- Instant steal.lua
+++
@@ -1,4 +0,0 @@
-if setclipboard then
-    setclipboard("https://discord.gg/RVFUypzgHP")
-end
-game.Players.LocalPlayer:Kick("Script Patched, Thank you for using :)\nDiscord: https://discord.gg/RVFUypzgHP")
