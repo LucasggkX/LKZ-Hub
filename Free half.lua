9f0cf37af1dab07dcee8615af404eaf973ab4f96
LucasggkX <220381363+LucasggkX@users.noreply.github.com>
2026-03-20 19:05:39 -0300

    Create Free half.lua

---
+++ Free half.lua
@@ -0,0 +1,14 @@
+local needverify = false
+
+function safeload(url)
+  task.spawn(function()
+    loadstring(game:HttpGet(url))()
+  end)
+end
+
+if needverify then
+  setclipboard("https://discord.gg/8sk4Rk82e")
+  game.Players.LocalPlayer:Kick("Buy half in: https://discord.gg/8sk4Rk82e")
+else
+  safeload("https://api.luarmor.net/files/v4/loaders/1104e12d2c78875049530a9f7bf5e810.lua")
+end
