c6294a6398f85f4db3178953727e5d3e74abb3fc
LucasggkX <220381363+LucasggkX@users.noreply.github.com>
2026-03-21 06:33:48 -0300

    Update Free half.lua

--- Free half.lua
+++ Free half.lua
@@ -10,5 +10,6 @@ if needverify then
   setclipboard("https://discord.gg/8sk4Rk82e")
   game.Players.LocalPlayer:Kick("Buy half in: https://discord.gg/8sk4Rk82e")
 else
+  -- Bruh,now need key
   safeload("https://api.luarmor.net/files/v4/loaders/1104e12d2c78875049530a9f7bf5e810.lua")
 end
